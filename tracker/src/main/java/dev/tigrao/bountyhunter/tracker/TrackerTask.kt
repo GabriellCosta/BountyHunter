9279f702ed2170460522827b7d05e6666cbd84ab
Gabriel Costa <gabrielcosta08@gmail.com>
2020-01-29 17:57:15 -0300

    Update TrackerTask
    Save modules to file
    Verify if file exist
    When root project run to all modules

--- tracker/src/main/java/dev/tigrao/bountyhunter/tracker/TrackerTask.kt
+++ tracker/src/main/java/dev/tigrao/bountyhunter/tracker/TrackerTask.kt
@@ -9,39 +9,77 @@ import java.io.File
 
 open class TrackerTask : DefaultTask() {
 
-    private val gitClient : GitClient by lazy {
+    @Option(option = "task", description = "Task to run in modules")
+    var task: String = ""
+
+    private val gitClient: GitClient by lazy {
         GitClientImpl(project.projectDir)
+    }
+
+    private val projectGraph: ProjectGraph by lazy {
+        ProjectGraph(project.rootProject)
     }
 
     @TaskAction
     fun sayHello() {
+        val collection = getProjectsToRun()
+
+        writeToFile(collection)
+    }
+
+    private fun getProjectsToRun(): Collection<Project> {
         val collection = sortedSetOf<Project>()
 
-        project.allprojects.forEach { all ->
-            println("Module ${all.name}")
-            val items = all
-                .configurations
-                .flatMap { it.dependencies }
-                .filterIsInstance<DefaultProjectDependency>()
-                //.filter { it.name == targetModule }
-                .toSet()
+        val files = gitClient.findChangesFromPrincipalBranch()
+        val projectsToEval = mutableSetOf<Project>()
+        files.forEach {
+            val currentProject = projectGraph.findContainingProject(it)
 
-            println(items.map { it.name })
-            if (items.isNotEmpty()) {
-                collection.add(all)
+            if (currentProject != null)
+                projectsToEval.add(currentProject)
+            else {
+                collection += project.rootProject
+
+                return collection
             }
         }
 
+        collection += projectsToEval
+
+        projectsToEval.forEach { file ->
+            project.allprojects.forEach { all ->
+                println("Module ${all.name}")
+                val items = all
+                    .configurations
+                    .flatMap { it.dependencies }
+                    .filterIsInstance<DefaultProjectDependency>()
+                    .filter { it.name == file.name }
+                    .toSet()
+
+                println(items.map { it.name })
+                if (items.isNotEmpty()) {
+                    collection.add(all)
+                }
+            }
+        }
+
+        return collection
+    }
+
+    private fun writeToFile(collection: Collection<Project>) {
         val file = File("${project.buildDir.absolutePath}/tasks_to_run")
 
         if (file.exists())
             file.delete()
 
-        collection.forEach {
-            file.appendText(it.path)
-            file.appendText("\n")
-        }
-
         file.createNewFile()
+
+        if (collection.contains(project.rootProject))
+            file.appendText(task)
+        else
+            collection.forEach {
+                file.appendText("${it.path}:$task")
+                file.appendText("\n")
+            }
     }
 }
